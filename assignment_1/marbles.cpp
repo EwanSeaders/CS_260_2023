88eefd21ff1d3188175bb02ea66d4930082187eb
EwanSeaders <122401941+EwanSeaders@users.noreply.github.com>
2023-04-15 20:49:42 -0700

    initial bag of marbles implementation attempt

---
+++ assignment_1/marbles.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <list>
+#include <random>
+#include <array>
+
+using namespace::std;
+
+// int insert(marble) {
+
+// }
+
+// void buildBag() {
+//     int marbles[10] = {0};
+//     return marbles;
+// }
+int addMarble(int* marble, int arrayLen) {
+    
+    }
+
+int removeMarble() {
+}
+
+
+int main() {
+    int arrayLen = 0;
+    int marbles[10] = {0};
+    marbles*
+    addMarble(marbles, arrayLen)
+    marbles[3] = 1;
+
+    // marbles.pop()
+    // marbles[5] = {"blue"};
+    for (int i = 0; i < sizeof marbles/sizeof marbles[1]; ++i) {
+        cout << "marbles["<< i <<"]: " << marbles[i] << endl;
+    }
+    // sizeof marbles/sizeof marbles[0]
+    // cout <<  marbles << endl;
+    return 0;
+}
+
+
